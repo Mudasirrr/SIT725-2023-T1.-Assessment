38bdea0ccbe2aabfda318245cd0bc7954a3874b6
Mudasir Hussain <40722738+Mudasirrr@users.noreply.github.com>
2023-07-23 16:13:15 +1000

    Update client.js

--- client/js/client.js
+++ client/js/client.js
@@ -1,4 +1,4 @@
 console.log('Client side.........');
 
-// Insert the 'Hey there!' text on the template page
+// Insert the 'Hey there!' 
 document.getElementById('greeting').innerText = 'Hey there! its my first task in Deakin University';
